44ff2a0bec699d5aa548882c6c3f80517499583a
xearts <xearts@user-no-MacBook-Pro.local>
2019-01-25 12:54:27 +0900

    show_prime_numbers.php was added

---
+++ functions/show_prime_numbers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xearts
+ * Date: 2019-01-25
+ * Time: 12:41
+ */
+
+include_once('PrimeNumbers.php');
+
+parse_str(implode('&', array_slice($argv, 1)), $_GET);
+
+$limit = $_GET['limit'];
+
+print_r(PrimeNumbers::findPrimeNumbers($limit));
