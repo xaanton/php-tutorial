4aaddbf430a7fcf9db22bbc0f9c00ff20ae6daf5
xearts <xearts@user-no-MacBook-Pro.local>
2019-01-25 10:28:55 +0900

    PrimeNumbers is a class with static methods now, code formatting

--- functions/PrimeNumbers.php
+++ functions/PrimeNumbers.php
@@ -6,23 +6,35 @@
  * Date: 2019-01-24
  * Time: 16:18
  */
-function find_prime_numbers($limit)
+
+class PrimeNumbers
 {
-    $result = array();
-    for($i = 0; $i <= $limit; $i++ ){
-        if(is_prime_number($i))array_push($result, $i);
+
+    public static function find_prime_numbers($limit)
+    {
+        $result = array();
+
+        for ($i = 0; $i <= $limit; $i++ ) {
+            if (PrimeNumbers::is_prime_number($i)) {
+                array_push($result, $i);
+            }
+        }
+
+        return $result;
     }
-    return $result;
-}
 
-function is_prime_number($number)
-{
-    if($number === 0 || $number === 1)return false;
+    public static function is_prime_number($number)
+    {
+        if ($number === 0 || $number === 1) {
+            return false;
+        }
+        $number_half = $number/2;
 
-    $number_half = $number/2;
-
-    for($i = 2; $i < $number_half; $i++)
-        if($number % $i == 0)return false;
-
-    return true;
+        for ($i = 2; $i < $number_half; $i++) {
+            if ($number % $i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
